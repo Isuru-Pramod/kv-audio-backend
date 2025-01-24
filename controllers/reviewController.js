80d6fb3700a2e404a63bf8fe12bc394ac325a9b2
Isuru-Pramod <mdipromod@gmail.com>
2025-01-24 14:31:58 +0530

    Review approve part is added

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -45,13 +45,57 @@ export function getReview(req,res){
 export function deleteReview (req,res){
     const email = req.params.email;
 
-    Review.deleteOne({email : email}).then(()=>{
-        res.json({message : "Review deleted"})
-    }).catch(()=>{
-        res.status(500).json({
-            error : "Review deletion dailed"
+    if (req.user == null) {
+        res.status(401).json({message : "plaese loging and try again"});
+        return;
+    }
+
+    if (req.user.role == "admin"){
+        Review.deleteOne({email : email}).then(()=>{
+            res.json({message : "Review deleted"})
+        }).catch(()=>{
+            res.status(500).json({
+                error : "Review deletion dailed"
+            });
+        })
+        return;
+    }
+
+    if (req.user.role == "customer"){
+        if (req.user.email == email){
+            Review.deleteOne({email : email}).then(()=>{
+                res.json({message : "Review deleted"})
+            }).catch(()=>{
+                res.status(500).json({
+                    error : "Review deletion failed"
+                });
+            })
+        }else{
+            res.status(403).json({message : "you are not authorized to perform this action"});
+        }
+    }
+
+}
+
+export function approveReview(req,res){
+    const email = req.params.email;
+
+    if (req.user == null){
+        res.status(401).json({
+            message : "plaese login and try again"
         });
-    })
+        return;
+    }
+
+    if (req.user.role == "admin"){
+        Review.updateOne({email : email,},{isApproved : true,}).then(()=>{
+            res.json({message : "Review approved successfully"})
+        }).catch(()=>{
+            res.status(500).json({error : "Review approval faild"})
+        })
+    }else{
+        res.status(403).json({ message : "You are not an admin. only admin can approve the reviews "});
+    }
 }
 
 
--- routes/ReviewRute.js
+++ routes/ReviewRute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addReview, getReview, deleteReview } from "../controllers/reviewController.js";
+import { addReview, getReview, deleteReview, approveReview } from "../controllers/reviewController.js";
 
 const reviewRoute = express.Router();
 reviewRoute.post("/",addReview);
 reviewRoute.get("/",getReview);
 reviewRoute.delete("/:email",deleteReview);
+reviewRoute.put("/approve/:email",approveReview);
 
 export default reviewRoute;
